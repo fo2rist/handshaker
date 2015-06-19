5ecbdeea8b5a860500d798c33562f54cdc5ae3ed
fo2rist <fo2@inbox.ru>
2015-06-20 00:29:20 +0300

    First version that shows access info in real time

--- handshaker_pebble/src/handshaker_pebble.c
+++ handshaker_pebble/src/handshaker_pebble.c
@@ -3,8 +3,52 @@
 static Window *window;
 static TextLayer *text_layer;
 
+// --------------------- BACKGROUND WORKER COMMUNICATION ---------
+static void worker_message_handler(uint16_t type, AppWorkerMessage *data) {
+  if (type == 0) {
+    // Read ticks from worker's packet
+    AccelData accel_data = {
+      .x = data->data0,
+      .y = data->data1,
+      .z = data->data2,
+      .did_vibrate = false
+    };
+    
+    // Long lived buffer
+    static char s_buffer[128];
+    
+    // Compose string of all data
+    snprintf(s_buffer, sizeof(s_buffer),
+             "X,Y,Z\n %d,%d,%d\n",
+             accel_data.x, accel_data.y, accel_data.z
+             );
+    
+    //Show the data
+    text_layer_set_text(text_layer, s_buffer);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Got %s", s_buffer);
+  }
+}
+
+// --------------------- CLICK LISTENERS CONFIG ------------------
+
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
+  AppWorkerResult result = app_worker_launch();
+  switch (result) {
+    case APP_WORKER_RESULT_SUCCESS:
+      text_layer_set_text(text_layer, "Runned");
+      break;
+
+    case APP_WORKER_RESULT_ALREADY_RUNNING:
+      text_layer_set_text(text_layer, "Already running.\nLet's stop.");
+      app_worker_kill();
+      break;
+
+    case APP_WORKER_RESULT_ASKING_CONFIRMATION:
+      //no special handling now
+    default:
+      text_layer_set_text(text_layer, "Failed");
+      break;
+  }
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -21,11 +65,13 @@ static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
 }
 
+// ----------------------- INIT PART ------------------------------
+
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
+  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 40 } });
   text_layer_set_text(text_layer, "Press a button");
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
@@ -36,6 +82,7 @@ static void window_unload(Window *window) {
 }
 
 static void init(void) {
+  //Show the window
   window = window_create();
   window_set_click_config_provider(window, click_config_provider);
   window_set_window_handlers(window, (WindowHandlers) {
@@ -44,10 +91,15 @@ static void init(void) {
   });
   const bool animated = true;
   window_stack_push(window, animated);
+  
+  //Subscribe to worker's messages
+  app_worker_message_subscribe(worker_message_handler);
 }
 
 static void deinit(void) {
   window_destroy(window);
+  // No more worker updates
+  app_worker_message_unsubscribe();
 }
 
 int main(void) {
--- handshaker_pebble/worker_src/handshaker_pebble_worker.c
+++ handshaker_pebble/worker_src/handshaker_pebble_worker.c
@@ -1,5 +1,33 @@
 #include <pebble_worker.h>
 
+
+static void data_handler(AccelData *data, uint32_t num_samples) {
+  // Construct a data packet
+  AppWorkerMessage msg_data = {
+    .data0 = data->x,
+    .data1 = data->y,
+    .data2 = data->z
+  };
+  
+  // Send the data to the foreground app
+  app_worker_send_message(0, &msg_data);
+}
+
+static void worker_init() {
+  // Subscribe to the accelerometer data service
+  int num_samples = 1;
+  accel_data_service_subscribe(num_samples, data_handler);
+  
+  // Choose update rate
+  accel_service_set_sampling_rate(ACCEL_SAMPLING_10HZ);
+}
+
+static void worker_deinit() {
+  accel_data_service_unsubscribe();
+}
+
 int main(void) {
+  worker_init();
   worker_event_loop();
+  worker_deinit();
 }
